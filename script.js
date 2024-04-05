c8798cf68c8333470b1b8b3f339885d9f5a964a9
cinnabun <109777617+cinnabunnnxd@users.noreply.github.com>
2024-04-05 09:22:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const options = {
+    bottom: '72px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#FFF2E1',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+
+darkmode.showWidget();
